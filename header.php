043c62bcdb72b7d999aeca6ca31ec532cf360c08
Nico <nicolas@Nios-MacBook.local>
2017-02-06 13:13:49 +0100

    Bugfixing, Cleanup

--- header.php
+++ header.php
@@ -4,10 +4,6 @@
 <meta charset="UTF-8">
 
 
-
-
-
-
 <meta content="minimum-scale=1.0, width=device-width, maximum-scale=1, user-scalable=no" name="viewport" />
 <link type="text/css" rel="stylesheet" href="<?php echo(get_template_directory_uri()); ?>/style.css"></link>
 <link type="text/css" rel="stylesheet" href="<?php echo(get_template_directory_uri()); ?>/css/layout.css"></link>
@@ -23,7 +19,7 @@
 
 
 <title><?php
-    wp_title(); 
+    wp_title();
     if ( is_front_page() == false){
         echo " - ";
     }
@@ -50,7 +46,7 @@
         <!-- Code to be includet...
     <!-- End Google Tag Manager -->
 <?php } ;?>
-    
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
 
@@ -78,57 +74,42 @@ $(document).ready( function(){
     });
 });
 
-console.log("Wie ist das wetter außerhalb von FuWa so? Wir sind im Schnee gefangen. Hilfe!!!")
 </script>
 
 
 
-
-
-  
-    
-</script>    
-    
-
-    
-    
 </head>
 
-<body> 
+<body>
     <script type="text/javascript" src="<?php echo(get_template_directory_uri()); ?>/PlugIns/flexnav-master/js/jquery.flexnav.min.js"></script>
-    <div id="header"> 
+    <div id="header">
         <div id="branding">
-            
-                
+
+
                     <a href="<?php bloginfo('url'); ?>">
-                        
+
                         <a id="PageTitle" href="<?php bloginfo('url'); ?>">
                             <?php bloginfo('name') ?>
                         </a>
                     </a>
                     <h2 id="SubHeadline"><?php echo(bloginfo('description')); ?></h2>
-                
-            
+
+
 
             <nav id="headernav">
                 <ul id="menueelemente">
-                    
+
                     <div id="menuebutton"><a href="#"><i class="icon-reorder"></i></a></div>
 
                     <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>
 
-                    
-                    
                 </ul>
-                
-                
-                
-                
-                
+
+
             </nav>
-         </div> 
+         </div>
     </div>
-    
+
     <div class="parallaxScrolling">
 	    <div id="colortransition"></div>
     </div>
